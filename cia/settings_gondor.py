ea3beda4f5c8a91955c8826432c00a76dc6c3fbe
Jos van Velzen <jos.vanvelzen@changer.nl>
2012-10-05 15:13:32 +0200

    Add gondor requirements

---
+++ cia/settings_gondor.py
@@ -0,0 +1,58 @@
+import os
+import urlparse
+
+from .settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+if "GONDOR_DATABASE_URL" in os.environ:
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["GONDOR_DATABASE_URL"])
+    DATABASES = {
+        "default": {
+            "ENGINE": {
+                "postgres": "django.db.backends.postgresql_psycopg2"
+            }[url.scheme],
+            "NAME": url.path[1:],
+            "USER": url.username,
+            "PASSWORD": url.password,
+            "HOST": url.hostname,
+            "PORT": url.port
+        }
+    }
+
+SITE_ID = 1 # set this to match your Sites setup
+
+STATIC_ROOT = os.path.join(os.environ["GONDOR_DATA_DIR"], "static")
+
+STATIC_URL = "/static/" # make sure this maps inside of site_media_url
+ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
+
+FILE_UPLOAD_PERMISSIONS = 0640
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "%(levelname)s %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "simple"
+        }
+    },
+    "loggers": {
+        "": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+        "django.request": {
+            "propagate": True,
+        },
+    }
+}
